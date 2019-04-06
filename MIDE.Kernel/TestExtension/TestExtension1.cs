9010885f7bd1ccedeba7e189483943d2660c904e
Andrew Satura <andriy.satura@gmail.com>
2019-04-06 21:46:36 +0200

    Updated application kernel version

--- MIDE.Kernel/TestExtension/TestExtension1.cs
+++ MIDE.Kernel/TestExtension/TestExtension1.cs
@@ -5,7 +5,7 @@ using MIDE.Application.Attrubites;
 
 namespace MIDE.Kernel.ConsoleTest.Extensions
 {
-    [Dependency(Version = "0.3.1", Type = DependencyType.ApplicationKernel)]
+    [Dependency(Version = "0.6.2", Type = DependencyType.ApplicationKernel)]
     [ExtensionProperties(Version = "1.0.0")]
     public class TestExtension1 : AppExtension
     {
